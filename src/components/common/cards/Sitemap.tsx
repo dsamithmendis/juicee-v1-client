1a20539efea4d5cc63561ad2fb49ff80c0ae94f5
dsamithmendis <samithmendis.01@gmail.com>
2025-10-18 20:08:00 +0530

    dev: home

--- src/components/common/cards/Sitemap.tsx
+++ src/components/common/cards/Sitemap.tsx
@@ -1,7 +1,10 @@
 const Sitemap = () => (
-  <div className="text-white">
-    <h2 className="text-xl font-bold mb-2">Sitemap</h2>
-    <ul className="space-y-1">
+  <div className="space-y-4 text-gray-800 items-center text-center">
+    <h2 className="text-4xl font-extrabold">Sitemap</h2>
+    <p className="text-gray-600">
+      Easy Access
+    </p>
+    <ul className="space-y-4">
       {["Home", "About Us", "Services", "Blog", "Contact Us"].map((item) => (
         <li key={item} className="hover:underline cursor-pointer">
           {item}
