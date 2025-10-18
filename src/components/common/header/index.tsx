2fff231491fec0a4f93f98e7c13813bae756e32a
dsamithmendis <samithmendis.01@gmail.com>
2025-10-18 23:05:38 +0530

    dev: home

--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -11,7 +11,6 @@ export default function Header() {
     <section className="mt-10 text-gray-800">
       <div className="flex justify-between items-center">
 
-        {/* Logo */}
         <div className="flex items-center gap-2">
           <Image
             src="/images/1.png"
@@ -23,7 +22,6 @@ export default function Header() {
           <span className="font-extrabold text-2xl">JUICEE</span>
         </div>
 
-        {/* Desktop Menu */}
         <ul className="hidden md:flex gap-8 text-md font-medium">
           <li className="hover:text-pink-600 cursor-pointer">Home</li>
           <li className="hover:text-pink-600 cursor-pointer">Menu</li>
@@ -31,21 +29,18 @@ export default function Header() {
           <li className="hover:text-pink-600 cursor-pointer">Contact</li>
         </ul>
 
-        {/* Desktop Sign Up */}
         <button className="hidden md:block border border-gray-700 px-4 py-2 rounded-lg hover:bg-gray-800 hover:text-white transition">
           Sign Up
         </button>
 
-        {/* Mobile Menu Icon */}
         <button
           className="md:hidden text-2xl"
           onClick={() => setMenuOpen(!menuOpen)}
         >
-          <RiMenuLine />
+          <RiMenuLine className="hover:text-pink-700 cursor-pointer"/>
         </button>
       </div>
 
-      {/* Mobile Menu Dropdown */}
       {menuOpen && (
         <ul className="flex flex-col mt-4 gap-4 md:hidden text-sm font-medium">
           <li className="hover:text-pink-600 cursor-pointer">Home</li>
