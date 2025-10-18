03d260d9e5287974305d69f12e44ee5d0d029864
dsamithmendis <samithmendis.01@gmail.com>
2025-10-18 19:11:03 +0530

    dev: home

--- src/components/pages/landing/why-choose-us-section.tsx
+++ src/components/pages/landing/why-choose-us-section.tsx
@@ -9,14 +9,12 @@ const WhyChooseUsSection = () => {
 
   return (
     <section className="w-full mt-10">
-      <div className="flex justify-between items-start w-full">
-        {/* Left Group: Text */}
-        <div className="flex-shrink-0 w-2/7">
-          <p className="text-4xl font-extrabold mb-2">
+      <div className="flex flex-col md:flex-row justify-between items-start w-full gap-10 md:gap-6">
+        <div className="w-full md:w-2/7">
+          <p className="text-4xl font-extrabold mb-3">
             Why you
-            <br className="hidden md:block" /> should{" "}
-            <br className="hidden md:block" />
-            choose us?
+            <br className="hidden md:block" /> should
+            <br className="hidden md:block" /> choose us?
           </p>
           <p className="text-gray-600">
             Our commitment to excellence in the realm of juices sets us apart
@@ -24,10 +22,12 @@ const WhyChooseUsSection = () => {
           </p>
         </div>
 
-        {/* Right Group: Cards */}
-        <div className="flex flex-wrap gap-4 justify-end w-5/7">
+        <div className="flex flex-col md:flex-row flex-wrap gap-6 w-full md:w-5/7 justify-center md:justify-end">
           {fruits.map((fruit, i) => (
-            <div key={i} className={`w-2/7 ${i === 1 ? "relative mt-32" : ""}`}>
+            <div
+              key={i}
+              className={`w-full md:w-2/7 ${i === 1 ? "md:relative md:mt-32" : ""}`}
+            >
               <FruitCard img={fruit.img} name={fruit.name} />
             </div>
           ))}
@@ -36,4 +36,5 @@ const WhyChooseUsSection = () => {
     </section>
   );
 };
+
 export default WhyChooseUsSection;
