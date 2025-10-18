8e81cee9508af17d0f76d2303e0bba134b2f070f
dsamithmendis <samithmendis.01@gmail.com>
2025-10-18 17:10:59 +0530

    dev: home

---
+++ src/components/common/cards/FruitCard.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import React from "react";
+
+interface FruitCardProps {
+  img: string;
+  name: string;
+}
+
+const FruitCard = ({ img, name }: FruitCardProps) => {
+  return (
+    <div className="bg-[#ffdbd2] rounded-2xl p-6 flex flex-col items-center justify-center shadow-md hover:shadow-lg transition">
+      <Image
+        src={img}
+        alt={name}
+        width={100}
+        height={100}
+        className="rounded-full mb-4"
+      />
+    </div>
+  );
+};
+
+export default FruitCard;
---
+++ src/components/common/cards/HeroImageCard.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import React from "react";
+
+interface HeroImageCardProps {
+  src: string;
+  alt: string;
+  width?: number;
+  height?: number;
+}
+
+const HeroImageCard = ({
+  src,
+  alt,
+  width = 400,
+  height = 400,
+}: HeroImageCardProps) => {
+  return (
+    <div className="rounded-3xl overflow-hidden drop-shadow-2xl">
+      <Image src={src} alt={alt} width={width} height={height} className="object-cover" />
+    </div>
+  );
+};
+
+export default HeroImageCard;
