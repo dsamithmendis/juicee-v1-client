f44edef1c2c89477a3a20ff32d4b431c6277fd93
dsamithmendis <samithmendis.01@gmail.com>
2025-10-18 16:32:02 +0530

    dev: home

---
+++ src/components/common/cards/Sitemap.tsx
@@ -0,0 +1,12 @@
+const Sitemap = () => (
+  <div className="text-white">
+    <h2 className="text-xl font-bold mb-2">Sitemap</h2>
+    <ul className="space-y-1">
+      {["Home", "About Us", "Services", "Blog", "Contact Us"].map((item) => (
+        <li key={item} className="hover:underline cursor-pointer">{item}</li>
+      ))}
+    </ul>
+  </div>
+);
+
+export default Sitemap;
