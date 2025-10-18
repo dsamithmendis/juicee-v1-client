b08f7fa38d300900b28ce60fc5069461df02ac79
dsamithmendis <samithmendis.01@gmail.com>
2025-10-18 21:11:48 +0530

    dev: home

--- src/components/common/cards/JuiceCard.tsx
+++ src/components/common/cards/JuiceCard.tsx
@@ -8,17 +8,17 @@ interface JuiceCardProps {
 }
 
 const JuiceCard = ({ name, description, price, imageSrc }: JuiceCardProps) => (
-  <div className="rounded-lg shadow-md p-4 text-center">
+  <div className="rounded-lg shadow-2xl p-8">
     <Image
       src={imageSrc}
       alt={name}
       width={1200}
       height={1200}
-      className="w-full h-40 object-cover rounded-md mb-3"
+      className="w-full h-80 object-cover rounded-md mb-3"
     />
     <h3 className="text-xl font-semibold">{name}</h3>
     <p className="text-gray-600">{description}</p>
-    <div className="mt-2 text-pink-600 font-bold">{price}</div>
+    <div className="mt-2 text-pink-600 font-bold">Rs. {price}/-</div>
   </div>
 );
 
