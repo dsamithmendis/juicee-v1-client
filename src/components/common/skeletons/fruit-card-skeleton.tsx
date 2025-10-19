2fd7e0afe8da36f3967bc8827e7860d5e2a38b2c
dsamithmendis <samithmendis.01@gmail.com>
2025-10-19 16:23:43 +0530

    dev: home

--- src/components/common/skeletons/fruit-card-skeleton.tsx
+++ src/components/common/skeletons/fruit-card-skeleton.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function FruitCardSkeleton() {
   return (
     <div className="rounded-2xl p-2 h-[480px] md:h-80 lg:p-4 w-full flex flex-col items-center justify-center shadow-2xl bg-gradient-to-b from-[#f7ede6] to-[#f0c4a5] animate-pulse overflow-hidden">
-      <div className="h-full w-full bg-gray-300 rounded-2xl" />
+      <div className="h-full w-full bg-zinc-200 rounded-2xl" />
     </div>
   );
 }
